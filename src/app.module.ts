f16e274a02434a3e86f74d669525d201cff51b79
datnd10 <datndhe172134@fpt.edu.vn>
2024-09-06 17:07:56 +0700

    queue

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ScheduleModule } from '@nestjs/schedule';
 import { TaskService } from './task/task.service';
+import { AudioModule } from './audio/audio.module';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
-  imports: [ScheduleModule.forRoot()],
+  imports: [ScheduleModule.forRoot(), BullModule.forRoot({
+    redis: {
+      host: 'localhost',
+      port: 6379,
+    },
+  }), AudioModule],
   controllers: [AppController],
   providers: [AppService, TaskService],
 })
