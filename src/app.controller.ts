7edc46371d92c8fa2e94e4865d2abba2ef847ea5
datnd10 <datndhe172134@fpt.edu.vn>
2024-09-06 16:41:21 +0700

    cookies

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, HttpStatus, ParseFilePipeBuilder, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, HttpStatus, ParseFilePipeBuilder, Post, Req, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { UserEntity } from './user.entity';
 import { User } from './user.decorator';
+import { Request, Response } from 'express';
 
 @Controller()
 export class AppController {
@@ -43,5 +44,23 @@ export class AppController {
   ) {
     console.log(user);
     return user;
+  }
+
+  @Get('get-cookie')
+  finndAll(@Req() req: Request) {
+    console.log(req.cookies);
+    return req.cookies;
+  }
+
+  @Get('set-cookie')
+  setCookie(
+    @Res({ passthrough: true })
+    response: Response,
+  ) {
+    response.cookie(
+      'userId',
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
+    );
+    response.send('Cookie Saved Successfully');
   }
 }
