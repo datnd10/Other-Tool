ff1ed328cd1830ae36f31270b3aa23c8dcd20e6f
datnd10 <datndhe172134@fpt.edu.vn>
2024-09-06 17:24:19 +0700

    streaming

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { TaskService } from './task/task.service';
 import { AudioModule } from './audio/audio.module';
 import { BullModule } from '@nestjs/bull';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import { FileController } from './file/file.controller';
 
 @Module({
   imports: [ScheduleModule.forRoot(), BullModule.forRoot({
@@ -14,7 +15,7 @@ import { EventEmitterModule } from '@nestjs/event-emitter';
       port: 6379,
     },
   }), AudioModule, EventEmitterModule.forRoot()],
-  controllers: [AppController],
+  controllers: [AppController, FileController],
   providers: [AppService, TaskService],
 })
 export class AppModule { }
